f492ad48ee7676e5a2a22f9cd1f1c87cc7a9ee8c
Abhijeet-Chandra <abhijeetutsav@gmail.com>
2025-09-06 20:16:55 +0530

    MUX16to1 using 4X1_conditional

--- Lab5/MUX16to1.v
+++ Lab5/MUX16to1.v
@@ -1,4 +1,4 @@
-`include "MUX4to1.v"
+`include "MUX4to1_conditional.v"
 module MUX16to1(w,s,f);
 
 input[0:15] w;
@@ -6,10 +6,10 @@ input[3:0] s;
 wire[0:3] c;
 output f;
 
-MUX4to1 MUX4to1_stage1a(w[0:3],s[1:0],c[0]);
-MUX4to1 MUX4to1_stage1b(w[4:7],s[1:0],c[1]);
-MUX4to1 MUX4to1_stage1c(w[8:11],s[1:0],c[2]);
-MUX4to1 MUX4to1_stage1d(w[12:15],s[1:0],c[3]);
-MUX4to1 MUX4to1_stage2(c,s[3:2],f);
+mux4to1_conditional MUX4to1_stage1a(w[0:3],s[1:0],c[0]);
+mux4to1_conditional MUX4to1_stage1b(w[4:7],s[1:0],c[1]);
+mux4to1_conditional MUX4to1_stage1c(w[8:11],s[1:0],c[2]);
+mux4to1_conditional MUX4to1_stage1d(w[12:15],s[1:0],c[3]);
+mux4to1_conditional MUX4to1_stage2(c,s[3:2],f);
 
 endmodule
