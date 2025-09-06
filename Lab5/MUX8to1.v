99cea894802be87e44079bb44d9caa278f38fc52
Abhijeet-Chandra <abhijeetutsav@gmail.com>
2025-09-06 20:11:09 +0530

    Update MUX8to1.v

--- Lab5/MUX8to1.v
+++ Lab5/MUX8to1.v
@@ -1,11 +1,19 @@
 module MUX8to1(w,s,f);
 
-input [0:7] w;
-input [2:0] s;
-
+input [0:7]w;
+input [2:0]s;
 output reg f;
 
-always @(w,s) begin
-    f = s[2] ? (s[1]?(s[0]?w[7]:w[6]):(s[0]?w[5]:w[4])):(s[1]?(s[0]?w[3]:w[2]):(s[0]?w[1]:w[0]));
-end
+always @(w,s,f) begin
+    case(s)
+    0: f = w[0];
+    1: f = w[1];
+    2: f = w[2];
+    3: f = w[3];
+    4: f = w[4];
+    5: f = w[5];
+    6: f = w[6];
+    7: f = w[7];
+    endcase
+end    
 endmodule
